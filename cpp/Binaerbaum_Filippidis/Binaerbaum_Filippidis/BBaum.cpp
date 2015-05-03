74a553c00754e1ca6e0a0c5640564c5889af6320
nikobellic760 <niko.filippidis@gmail.com>
2015-05-03 19:16:01 +0200

    Änderung in der Main

--- cpp/Binaerbaum_Filippidis/Binaerbaum_Filippidis/BBaum.cpp
+++ cpp/Binaerbaum_Filippidis/Binaerbaum_Filippidis/BBaum.cpp
@@ -83,23 +83,16 @@ void Inorder(Node *root) {
 }
 
 int main() {
-	/*Code To Test the logic
-	Creating an example tree
-	5
-	/ \
-	3   10
-	/ \   \
-	1   4   11
-	*/
-	Node* root = NULL;
-	root = Insert(root, 5); root = Insert(root, 10);
-	root = Insert(root, 3); root = Insert(root, 4);
-	root = Insert(root, 1); root = Insert(root, 11);
 
-	// Deleting node with value 5, change this value to test other cases
+	Node* root = NULL;
+	root = Insert(root, 5); 
+	root = Insert(root, 1); 
+	root = Insert(root, 11);
+
+
 	root = Delete(root, 5);
 
-	//Print Nodes in Inorder
+
 	cout << "Inorder: ";
 	Inorder(root);
 	cout << "\n";
